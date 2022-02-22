1b63cfcf123d49213718dd5a7762759b271a9b37
jalizadeh <jalizade3d@yahoo.com>
2022-02-22 20:58:21 +0100

    UI completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,8 +13,21 @@ const Navbar = () => {
             <Typography.Title level={2} className='logo'>
                 <Link to='/'>Cryptoverse</Link>
             </Typography.Title>
-            {/* <Button className='menu-control-container'></Button> */}
         </div>
+        <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined/>} key='1'>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined/>} key='2'>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined/>} key='3'>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined/>} key='4'>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
     </div>
   )
 }
